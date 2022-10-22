f95c28cf40ae90b861015ca671eb8a0eed8760f1
Rodrigo Cabral <digicabral@gmail.com>
2022-10-21 22:08:27 -0300

    CRUD with raw sql inside database

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
-import enum
+from dataclasses import dataclass
+import psycopg2 
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi import Body
 from pydantic import BaseModel
 from random import randrange
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
@@ -11,21 +14,17 @@ class Post(BaseModel):
         title: str
         content: str
         published: bool = True
-        rating: Optional[int] = None
 
-
-my_posts = [{"title":"Title1", "content":"content1", "id":1},
-            {"title":"Title2", "content":"content2", "id":2},
-            {"title":"Title3", "content":"content3", "id":3}]
-
-def find_post(id):
-        for p in my_posts:
-                if p["id"]==id:
-                        return p
-def find_post_index(id):
-        for i, p in enumerate(my_posts):
-                if p['id'] == id:
-                        return i
+while True:
+        try:
+                conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', password='admin', cursor_factory=RealDictCursor)
+                cursor = conn.cursor()
+                print("Database connection was successfull!")
+                break
+        except Exception as error:
+                print("Connecting to database failed")
+                print("Error: ", error)
+                time.sleep(2)
 
 @app.get("/")
 async def root():
@@ -33,18 +32,23 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-        return{"data": my_posts}
+        posts = cursor.execute(""" SELECT * FROM posts """)
+        posts = cursor.fetchall()
+        return{"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_posts(post: Post):
-        post_dict = post.dict()
-        post_dict['id'] = randrange(0,100000)
-        my_posts.append(post_dict)
-        return{"data":post_dict}
+        cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING 
+        * """, 
+                       (post.title, post.content, post.published))
+        new_post = cursor.fetchone()
+        conn.commit()
+        return{"data": new_post}
 
 @app.get("/posts/{id}")
 async def get_post(id: int):
-        post = find_post(id)
+        cursor.execute(""" SELECT * FROM posts WHERE ID = %s """, (str(id)))
+        post = cursor.fetchone()
         if not post:
                 raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
                                     detail=f"post with id: {id} was not found")
@@ -52,20 +56,24 @@ async def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int):
-        index = find_post_index(id)
-        if index == None:
+        cursor.execute(""" DELETE FROM posts WHERE ID = %s RETURNING * """, (str(id)))
+        deleted_post = cursor.fetchone()
+        conn.commit()
+        
+        if deleted_post == None:
                 raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
                                     detail=f"post with id: {id} was not found")
-        my_posts.pop(index)
+
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 async def update_post(id: int, post: Post):
-        index = find_post_index(id)
-        if index == None:
+        
+        cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE ID = %s RETURNING * """, (post.title, post.content, post.published, str(id) ))
+        updated_post = cursor.fetchone()
+        conn.commit()
+        if updated_post == None:
                 raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
                                     detail=f"post with id: {id} was not found")
-        post_dict = post.dict()
-        post_dict['id'] = id
-        my_posts[index] = post_dict
-        return {'data': post_dict}
+
+        return {'data': updated_post}
