04bb619c45d9cb2590538b7d1a73767baf0000bc
Rodrigo Cabral <digicabral@gmail.com>
2022-10-27 18:23:08 -0300

    Added models to responses

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 import psycopg2 
-from typing import Optional
+from typing import List
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi import Body
 
@@ -14,7 +14,6 @@ from .database import engine, get_db
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 
 while True:
         try:
@@ -31,26 +30,26 @@ while True:
 async def root():
         return{"message":"Hello world!!"}
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 async def get_posts(db: Session = Depends(get_db)):
         posts = db.query(models.Post).all()
-        return {"data": posts}
+        return posts
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-async def create_posts(post: schemas.Post, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+async def create_posts(post: schemas.PostBase, db: Session = Depends(get_db)):
         new_post = models.Post(**post.dict())
         db.add(new_post)
         db.commit()
         db.refresh(new_post)
-        return{"data": new_post}
+        return new_post
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 async def get_post(id: int, db: Session = Depends(get_db)):
         post = db.query(models.Post).filter(models.Post.id == id).first()
         if not post:
                 raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
                                     detail=f"post with id: {id} was not found")
-        return {"data": post}
+        return post
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: int, db: Session = Depends(get_db)):
@@ -62,8 +61,8 @@ async def delete_post(id: int, db: Session = Depends(get_db)):
         db.commit()
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
-async def update_post(id: int, updated_post: schemas.Post, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.Post)
+async def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
         
         post_query = db.query(models.Post).filter(models.Post.id == id)
         post = post_query.first()
@@ -75,4 +74,4 @@ async def update_post(id: int, updated_post: schemas.Post, db: Session = Depends
         post_query.update(updated_post.dict(), synchronize_session=False)
         db.commit()
 
-        return {'data': post_query.first()}
+        return post_query.first()
