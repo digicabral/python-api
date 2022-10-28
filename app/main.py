21758057c6665ede1c11c9685b7e38c6c3d88f98
Rodrigo Cabral <digicabral@gmail.com>
2022-10-28 19:53:27 -0300

    Creating Users

--- app/main.py
+++ app/main.py
@@ -75,3 +75,11 @@ async def update_post(id: int, updated_post: schemas.PostCreate, db: Session = D
         db.commit()
 
         return post_query.first()
+
+@app.post("/users", status_code=status.HTTP_201_CREATED)
+async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+        new_user = models.User(**user.dict())
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        return new_user
