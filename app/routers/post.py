f030d061ca8a77a17f7e9ad37f65b741055aa211
Rodrigo Cabral <digicabral@gmail.com>
2022-11-07 20:02:57 -0300

    Pagination and query parameters

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import models, schemas, oauth2
 from sqlalchemy.orm import Session
@@ -10,8 +10,9 @@ router = APIRouter(
 )
 
 @router.get("/", response_model=List[schemas.Post])
-async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
-        posts = db.query(models.Post).filter(models.Post.owner_id==current_user.id).all()
+async def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
+limit: int = 10, skip: int = 0, search: Optional[str]=""):
+        posts = db.query(models.Post).filter(models.Post.owner_id==current_user.id, models.Post.title.contains(search)).limit(limit).offset(skip).all()
         return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
